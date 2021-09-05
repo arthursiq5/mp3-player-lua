122dfeab4f2a92826c5ab3dfefb277a82dd861c7
arthursiq5 <arthur.siq5@gmail.com>
2021-09-05 17:11:26 -0300

    Reproduzida música

--- main.lua
+++ main.lua
@@ -1,3 +1,4 @@
+som = love.audio.newSource( 'audio/Unexplored_Moon-Miguel_Johnson.mp3', 'static' )
 imgPlay = love.graphics.newImage( 'img/play.png' )
 imgPause = love.graphics.newImage( 'img/pause.png' )
 imgStop = love.graphics.newImage( 'img/stop.png' )
@@ -5,7 +6,8 @@ volume = 1
 local textoVolume = 'teste'
 
 function love.update( dt )
-
+    som:setVolume( volume )
+    textoVolume = volume
 end
 
 function love.draw()
@@ -17,3 +19,16 @@ function love.draw()
     love.graphics.draw( imgPause, 200, 50, 0 )
     love.graphics.draw( imgStop, 300, 50, 0 )
 end
+
+function love.mousepressed(mx, my, button)
+    if button == 1 and mx >= 100 and mx < 100 + imgPlay:getWidth() and my >= 50 and my < 50 + imgPlay:getHeight() then
+        som:play()
+    end
+    if button == 1 and mx >= 200 and mx < 200 + imgPause:getWidth() and my >= 50 and my < 50 + imgPause:getHeight() then
+        som:pause()
+    end
+    if button == 1 and mx >= 300 and mx < 300 + imgStop:getWidth() and my >= 50 and my < 50 + imgStop:getHeight() then
+        som:stop()
+    end
+end
+
