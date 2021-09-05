d54572af27601af6da60b4ae62a283bf877fcc32
arthursiq5 <arthur.siq5@gmail.com>
2021-09-05 00:12:30 -0300

    Criada estrutura básica do player

---
+++ main.lua
@@ -0,0 +1,13 @@
+imgPlay = love.graphics.newImage( 'img/play.png' )
+imgPause = love.graphics.newImage( 'img/pause.png' )
+imgStop = love.graphics.newImage( 'img/stop.png' )
+volume = 1
+local textoVolume = ''
+
+function love.update( dt )
+
+end
+
+function love.draw()
+
+end
