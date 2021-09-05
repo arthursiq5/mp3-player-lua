b6501f14256ce01b74d2ef99aa6ef4228321a915
arthursiq5 <arthur.siq5@gmail.com>
2021-09-05 16:46:42 -0300

    Criado design do player

--- main.lua
+++ main.lua
@@ -2,12 +2,18 @@ imgPlay = love.graphics.newImage( 'img/play.png' )
 imgPause = love.graphics.newImage( 'img/pause.png' )
 imgStop = love.graphics.newImage( 'img/stop.png' )
 volume = 1
-local textoVolume = ''
+local textoVolume = 'teste'
 
 function love.update( dt )
 
 end
 
 function love.draw()
-
+    love.graphics.setColor( 0, 0, 0 )
+    love.graphics.print( textoVolume, 50, 30 )
+    love.graphics.setColor( 255, 255, 255 )
+    love.graphics.setBackgroundColor( 255, 255, 255 )
+    love.graphics.draw( imgPlay, 100, 50, 0 )
+    love.graphics.draw( imgPause, 200, 50, 0 )
+    love.graphics.draw( imgStop, 300, 50, 0 )
 end
